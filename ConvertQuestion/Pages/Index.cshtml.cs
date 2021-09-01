32541b0c010db13b767a6ed6cc24249137bde46b
pomoyen <89518716+pomoyen@users.noreply.github.com>
2021-09-01 16:42:21 +0200

    test
    test

--- ConvertQuestion/Pages/Index.cshtml.cs
+++ ConvertQuestion/Pages/Index.cshtml.cs
@@ -4,13 +4,17 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace ConvertQuestion.Pages
 {
     public class IndexModel : PageModel
     {
+        static HttpClient client = new HttpClient();
         private readonly ILogger<IndexModel> _logger;
+        public string Chaine { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -19,7 +23,23 @@ namespace ConvertQuestion.Pages
 
         public void OnGet()
         {
+
+           Chaine =  Getstring().GetAwaiter().GetResult();
             
+        }
+
+        public async Task<string> Getstring()
+        {
+            string response = string.Empty;
+            using (var client = new HttpClient())
+            {
+                 response =
+                    await client.GetStringAsync("http://appliweather-jhtpro-dev.apps.sandbox.x8i5.p1.openshiftapps.com/weatherforecast");
+                //The response object is a string that looks like this:
+                //"{ message: 'Hello world!' }"
+            }
+            
+            return response;
         }
     }
 }
